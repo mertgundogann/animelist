8aae78b2a3d5b2094f8cca1e62c96df821178284
Mert <merttgundogan@hotmail.com>
2025-12-06 04:44:12 +0300

    view engine

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
